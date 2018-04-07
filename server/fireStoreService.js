1c9e37075860380e9b057274f6681c6479168b2c
guangLess <bha.guang@gmail.com>
2018-04-06 22:05:45 -0400

    testing firestore methods

--- server/fireStoreService.js
+++ server/fireStoreService.js
@@ -1,3 +1,4 @@
+
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 const firebase = require('firebase')
@@ -10,7 +11,7 @@ admin.initializeApp({
 });
 
 const firestore = admin.firestore();
-const ref = firestore.doc('samples/data')
+const ref = firestore.collection('food')
 
 /*
 ref.set({
@@ -19,9 +20,60 @@ ref.set({
   .catch(error => console.log('Got an error: ', error))
 */
 
-ref.get().then(doc => {
-    if (doc && doc.exists){
-        const data = doc.data()
-        console.log('recieved data =', data)
-    }
-}).catch(console.log)
+
+//console.log(' hello ref ===', ref)
+//ref.doc('apple').get().then(snapshot => {
+    //console.log("snap====>", snapshot.data())
+   // if (snapshot && snapshot.exists){
+    //   snapshot.forEach( doc => {
+    //     const data = doc.data()
+    //     console.log('recieved data =', data, doc.id)
+    //   })
+    //}
+//}).catch(console.log)
+
+
+// const apple =  ref.where('name', '==', 'apple').get()
+// .then(snap => {
+//     console.log(snap)
+// })
+// console.log('====>>>', apple)
+
+//doc apple
+const onion = 'onion'
+const onionData = {
+    name: ' onion',
+    status: {
+        yes: false,
+        but: 'just no, never, bad, bad, bad!'
+    },
+    type: 'veggie',
+    url: 'https://www.cesarsway.com/dog-care/nutrition/dog-approved-people-food'
+}
+
+const apple = 'apple'
+const appleData = {
+    name: ' apple',
+    status: {
+        yes: true,
+        but: 'carefule with core && seeds'
+    },
+    type: 'fruit',
+    url: 'https://www.cesarsway.com/dog-care/nutrition/dog-approved-people-food'
+}
+
+const greenbean = 'greenbean'
+const greenbeanData = {
+    name: ' greenbean',
+    status: {
+        yes: true,
+        but: 'no salt or spieces tho!'
+    },
+    type: 'veggie',
+    url: 'https://www.cesarsway.com/dog-care/nutrition/dog-approved-people-food'
+}
+
+
+ref.doc(apple).set(appleData)
+ref.doc(greenbean).set(greenbeanData)
+ref.doc(onion).set(onionData)
