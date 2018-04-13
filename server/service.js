e27afb4ecbe0f8a942ad00a538c155a0831d43ab
guangLess <bha.guang@gmail.com>
2018-04-13 17:01:58 -0400

    backend stuff withr react setups

--- server/service.js
+++ server/service.js
@@ -4,9 +4,9 @@ const {resolve} = require('path')
 const app = express()
 
 module.exports = app
-    .use(express.static(resolve(__dirname, '..', 'public')))
+    .use(express.static(resolve(__dirname, '..', 'dist')))
     .use('/api', require('./api'))
-    .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
+    .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'dist', 'index.html')))
 
 if (module === require.main) {
     const port = 3000
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import App from "./app"
+import style from "./main.css"
 
 console.log('webpack entry point🍯')
-
 const arr = [5,4,3]
 const esSix = () => console.log(...arr)
 window.esSix = esSix
+
