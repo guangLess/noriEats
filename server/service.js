03aa6a18bd3b411ea4bb1d518583e563147ef9c5
guangLess <bha.guang@gmail.com>
2018-04-19 11:07:16 -0400

    added session

--- server/service.js
+++ server/service.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require('express')
+const session = require('express-session')
 const {resolve, join} = require('path')
 
 const app = express()
+const cookie = {secret: 'token-key', cookie: { maxAge: 5000 }}
+app.use(session({cookie}))
+
 
 module.exports = app
     .use(express.static(resolve(__dirname, '..', 'dist')))
